463ab2f0054ca6ff99d3becc3398ba4a2fa226b2
long <1143403457@qq.com>
2021-10-14 17:50:40 +0800

    完成user数据库查询

--- apiserver/db/index.js
+++ apiserver/db/index.js
@@ -4,9 +4,9 @@ const mysql = require('mysql')
 const pool  = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
-    password : 'root',
+    password : 'long',
     // port: 3306,
-    database: 'h52108',
+    database: 'user',
 
     // 允许每个mysql语句有多条查询（默认false）.使用它时要非常注意，因为它很容易引起sql注入攻击
     // multipleStatements: true
--- apiserver/router/user.js
+++ apiserver/router/user.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const router = express.Router();
 module.exports = router;
+
+const db = require('../db')
+const {
+    formatData
+} = require('../utils')
+
+router.get('/list', async (req, res) => {
+    const sql = `select * from student`
+    const data = await db(sql)
+
+    res.send(formatData.success(data))
+})
